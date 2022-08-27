3a6ece80dbee483b824483484fe57ba915297ce8
oleg-shin <qaolegs@gmail.com>
2022-08-27 13:08:05 -0400

    feature sun added, but with Errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,10 +3,21 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         solarSystem inception = new solarSystem();
-        inception.Sun =1;
-        inception.Moon=4;
-        inception.Planets=2;
+        inception.Sun = 1;
+        inception.Moon = 4;
+        inception.Planets = 2;
 
         System.out.println("the basic structure is ready");
+
+
+//    developing the feature SUN
+        SUN omaro=new SUN();
+        omaro.temperature="12334433kj";
+        omaro.name="ferro";
+        omaro.radius=12344555454545455454;
+        omaro.color="pink";
     }
+
+    }
+
 }
