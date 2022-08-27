3e249f58dd311f2ebdfb83294284f29d4321c416
oleg-shin <qaolegs@gmail.com>
2022-08-27 12:28:39 -0400

    solar system ready to be deployed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,11 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        solarSystem inception = new solarSystem();
+        inception.Sun =1;
+        inception.Moon=4;
+        inception.Planets=2;
 
+        System.out.println("the basic structure is ready");
     }
 }
