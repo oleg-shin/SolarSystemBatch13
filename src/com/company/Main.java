7972c9dfbd0260185fc0e26f19f81c587cb47024
oleg-shin <qaolegs@gmail.com>
2022-08-28 11:16:00 -0400

    feature planet2 was added with 0 errors

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,13 +11,19 @@ public class Main {
 
 
 //    developing the feature SUN
-        SUN omaro=new SUN();
-        omaro.temperature="12334433kj";
-        omaro.name="ferro";
-        omaro.radius=1234455;
-        omaro.color="pink";
-    }
+        SUN omaro = new SUN();
+        omaro.temperature = "12334433kj";
+        omaro.name = "ferro";
+        omaro.radius = 1234455;
+        omaro.color = "pink";
 
+//    planet2
+        Planet2 superwomen = new Planet2();
+        superwomen.size = "766585857km";
+        superwomen.name = "pliea";
+        superwomen.color = "green";
     }
 
 }
+
+
