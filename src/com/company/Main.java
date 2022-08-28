1d032bb87703fa81b815b1f49c80c779a59e3c2a
oleg-shin <qaolegs@gmail.com>
2022-08-28 10:53:44 -0400

    planet1 added and is error free

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,13 +11,19 @@ public class Main {
 
 
 //    developing the feature SUN
-        SUN omaro=new SUN();
-        omaro.temperature="12334433kj";
-        omaro.name="ferro";
-        omaro.radius=1234455;
-        omaro.color="pink";
-    }
+        SUN omaro = new SUN();
+        omaro.temperature = "12334433kj";
+        omaro.name = "ferro";
+        omaro.radius = 1234455;
+        omaro.color = "pink";
 
+//        developing planet1
+        Planet1 superplanet=new Planet1();
+        superplanet.color="magenta";
+        superplanet.name="hash";
+        superplanet.size="545488km";
     }
 
 }
+
+
